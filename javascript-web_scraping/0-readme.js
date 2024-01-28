e362706ddece25a2657986ba4553857a71f97ad5
esteban-silvera <estebansilvera000@gmail.com>
2024-01-28 16:44:33 -0300

    .\0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-    console.log(error || content); 
+const process = require('process');
+
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
