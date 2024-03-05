02e4d422bcc92fbb757994676641b7d0893c7833
esteban-silvera <estebansilvera000@gmail.com>
2024-03-05 19:37:15 -0300

    7 fix lint

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -9,15 +9,15 @@ app.get('/', (_req, res) => {
 });
 
 app.get('/students', async (_req, res) => {
-res.write('This is the list of our students\n');
-await countStudents(process.argv[2])
-  .then((data) => {
-    const fields = Object.keys(data);
-    const total = fields.reduce(
-      (acc, curr) => acc + data[curr].numStudents,
-      0,
-    );
-    res.write(`Number of students: ${total}\n`);
+  res.write('This is the list of our students\n');
+  await countStudents(process.argv[2])
+    .then((data) => {
+      const fields = Object.keys(data);
+      const total = fields.reduce(
+        (acc, curr) => acc + data[curr].numStudents,
+        0,
+      );
+      res.write(`Number of students: ${total}\n`);
     for (let i = 0; i < fields.length; i += 1) {
       res.write(
         `Number of students in ${fields[i]}: ${
