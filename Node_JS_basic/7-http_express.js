7637e89947663c0e22b628bce9eaf42a06b5d9d4
esteban-silvera <estebansilvera000@gmail.com>
2024-03-05 19:34:14 -0300

    fix lint

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -9,33 +9,33 @@ app.get('/', (_req, res) => {
 });
 
 app.get('/students', async (_req, res) => {
-  res.write('This is the list of our students\n');
-  await countStudents(process.argv[2])
-    .then((data) => {
-      const fields = Object.keys(data);
-      const total = fields.reduce(
-        (acc, curr) => acc + data[curr].numStudents,
-        0,
+res.write('This is the list of our students\n');
+await countStudents(process.argv[2])
+  .then((data) => {
+    const fields = Object.keys(data);
+    const total = fields.reduce(
+      (acc, curr) => acc + data[curr].numStudents,
+      0,
+    );
+    res.write(`Number of students: ${total}\n`);
+    for (let i = 0; i < fields.length; i += 1) {
+      res.write(
+        `Number of students in ${fields[i]}: ${
+          data[fields[i]].numStudents
+        }. `,
       );
-      res.write(`Number of students: ${total}\n`);
-      for (let i = 0; i < fields.length; i += 1) {
-        res.write(
-          `Number of students in ${fields[i]}: ${
-            data[fields[i]].numStudents
-          }. `,
-        );
-        res.write(`List: ${data[fields[i]].names.join(', ')}`);
-        if (i < fields.length - 1) {
-          res.write('\n');
-        }
+      res.write(`List: ${data[fields[i]].names.join(', ')}`);
+      if (i < fields.length - 1) {
+        res.write('\n');
       }
-    })
-    .catch((err) => {
-      res.write(err.message);
-    })
-    .finally(() => {
-      res.end();
-    });
+    }
+  })
+  .catch((err) => {
+    res.write(err.message);
+  })
+  .finally(() => {
+    res.end();
+  });
 });
 app.listen(port);
 
